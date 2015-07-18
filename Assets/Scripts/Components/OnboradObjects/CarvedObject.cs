71e3036da82fd4e51a455381199a247ca0c12415
Freddie Wang <freddiew@qq.com>
2015-07-18 08:37:31 -0700

    Add Anti-Cheat Toolkits & Change to VS code

---
+++ Assets/Scripts/Components/OnboradObjects/CarvedObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// 刻石组件
+/// </summary>
+public class CarvedObject : MonoBehaviour
+{
+    #region 参数
+    public int Health { get; set; }
+    public int Power { get; set; }
+    public int Agility { get; set; }
+    public int Harm { get; set; }
+    public int Booster { get; set; }
+    #endregion
+}
