ff4c5877133b05047a9e853bb290369c70a9546d
Freddie Wang <freddiew@qq.com>
2015-09-02 22:49:48 -0700

    风格修正

--- Assets/Scripts/Classes/Deck.cs
+++ Assets/Scripts/Classes/Deck.cs
@@ -2,55 +2,53 @@
 
 namespace TickTick
 {
-	public class Deck {
+    public class Deck
+    {
 
-		int[] cardArr;
-		int cardCount;
+        int[] cardArr;
+        int cardCount;
 
-		/// <summary>
-		/// 临时生成的全卡组 TODO: Fuck this
-		/// </summary>
-		public Deck()
-		{
-			cardArr = new int[100];
-			for (int i = 0; i < 100; i++)
-			{
-				cardArr[i] = i;
-			}
-			cardCount = 0;
-		}
+        /// <summary>
+        /// 临时生成的全卡组 TODO: Fuck this
+        /// </summary>
+        public Deck()
+        {
+            cardArr = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                cardArr[i] = i;
+            }
+            cardCount = 0;
+        }
 
-		/// <summary>
-		/// 洗牌函数
-		/// </summary>
-		public void Shuffle()
-		{
+        /// <summary>
+        /// 洗牌函数
+        /// </summary>
+        public void Shuffle()
+        {
             int m;
-            int len = cardArr.Length;
+            var len = cardArr.Length;
             Random rand = new Random();
-            for (int x = 0; x < len +1; x++)
+            for (int x = 0; x < len + 1; x++)
             {
                 m = rand.Next(0, len);
                 if (m != 0)
                 {
-                    int temp = 0;
-                    temp = cardArr[0];
+                    var temp = cardArr[0];
                     cardArr[0] = cardArr[m];
                     cardArr[m] = temp;
-                } 
+                }
             }
+        }
 
-			// TODO: 不洗牌的黑箱
-		}
-
-		/// <summary>
-		/// 抽牌函数 TODO: Fuck this
-		/// </summary>
-		public int Draw()
-		{
-			cardCount += 1;
-			return cardArr[cardCount - 1];
-		}
-	}
+        /// <summary>
+        /// 抽牌函数 TODO: Fuck this
+        /// </summary>
+        public int Draw()
+        {
+            cardCount += 1;
+            return cardArr[cardCount - 1];
+        }
+    }
 }
 
